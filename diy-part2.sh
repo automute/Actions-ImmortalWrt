d6fa20849c689ca366c64658a19015c2e846bbb9
automute <wanglics@vip.qq.com>
2025-07-11 23:04:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -39,8 +39,8 @@ curl -L https://github.com/sbwml/openwrt_helloworld/raw/refs/heads/v5/shadowsock
 #git clone https://github.com/Ljzkirito/adguardhome-openwrt feeds/packages/net/adguardhome
 
 # Replace luci-theme-argon
-rm -rfv feeds/luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+# rm -rfv feeds/luci/themes/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
 # Replace natmap
 rm -rfv feeds/packages/net/natmap
@@ -52,7 +52,7 @@ git clone --depth 1 https://github.com/muink/luci-app-natmapt.git package/luci-a
 rm -rfv feeds/helloworld/dns2socks-rust
 rm -rfv feeds/helloworld/v2raya
 
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 #git clone --depth=1 -b master https://github.com/fw876/helloworld
 #Replace_package="xray-core xray-plugin v2ray-core v2ray-plugin hysteria ipt2socks microsocks redsocks2 chinadns-ng dns2socks dns2tcp naiveproxy simple-obfs tcping tuic-client"
